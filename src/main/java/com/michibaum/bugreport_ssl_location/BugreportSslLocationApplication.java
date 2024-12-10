061ab4645bf7cd79198eb1adb0010394b1437b8a
Michael Baumberger <michael_baumberger@gmx.ch>
2024-12-10 10:27:14 +0100

    Initialize Spring Boot project with Maven wrapper.
    This commit sets up a new Spring Boot application named "bugreport-ssl-location". It includes the addition of the Maven wrapper scripts, a basic `pom.xml` for managing dependencies, and a `.gitignore` to exclude build artifacts and IDE configurations. A default `application.properties` file is also added to name the application.

---
+++ src/main/java/com/michibaum/bugreport_ssl_location/BugreportSslLocationApplication.java
@@ -0,0 +1,13 @@
+package com.michibaum.bugreport_ssl_location;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BugreportSslLocationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BugreportSslLocationApplication.class, args);
+	}
+
+}
